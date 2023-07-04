664d564a506846bd8d8164e1bb48db01ceba1c63
JuHyeong158 <110215982+JuHyeong158@users.noreply.github.com>
2023-07-04 17:58:33 +0900

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,10 @@
+﻿const now = new Date();
+const today = {
+    year : now.getFullYear(),
+    month : now.getMonth()+1,
+    date : now.getDate()
+}
+
+for (let key in today) {
+    document.getElementById(key).textContent = today[key];
+}
